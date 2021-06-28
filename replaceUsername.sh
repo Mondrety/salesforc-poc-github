693e491b48dee6821f3c4acf7f1418803ec37c74
vidhyalakshmikalyan <vidhyalakshmi.k@ba.com>
2021-06-28 15:20:01 +0100

    changes in shellscript

--- replaceUsername.sh
+++ replaceUsername.sh
@@ -1,2 +1,3 @@
-#filepath=\salesforce-poc\force-app\main\default\authproviders\AwsAuthProvider.authprovider-meta.xml
-sed -i 's/<executionUser>vidhyalakshmi.k@ba.com.bisdev<\/executionUser>/<executionUser>vidhyalakshmi.k@ba.com.devops<\/executionUser>/' -d force-app/main/default/authproviders/AwsAuthProvider.authprovider-meta.xml
+#filepath=\force-app\main\default\authproviders\AwsAuthProvider.authprovider-meta.xml
+filepath='/force-app/main/default/authproviders/AwsAuthProvider.authprovider-meta.xml'
+sed -i 's/<executionUser>vidhyalakshmi.k@ba.com.bisdev<\/executionUser>/<executionUser>vidhyalakshmi.k@ba.com.devops<\/executionUser>/' $filepath
