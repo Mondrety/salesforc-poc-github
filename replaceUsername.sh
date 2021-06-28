fcaa78e030964a7d390c8d57bc37852fec36f367
vidhyalakshmikalyan <vidhyalakshmi.k@ba.com>
2021-06-28 07:18:35 +0100

    changes in shellscript

--- replaceUsername.sh
+++ replaceUsername.sh
@@ -1,1 +1,2 @@
-sed -i 's/<executionUser>vidhyalakshmi.k@ba.com.bisdev</executionUser>/<executionUser>vidhyalakshmi.k@ba.com.devops</executionUser>/' force-app\main\default\authproviders\AwsAuthProvider.authprovider-meta.xml
+filepath=\force-app\main\default\authproviders\AwsAuthProvider.authprovider-meta.xml
+sed -i 's/<executionUser>vidhyalakshmi.k@ba.com.bisdev</executionUser>/<executionUser>vidhyalakshmi.k@ba.com.devops</executionUser>/' $filepath
