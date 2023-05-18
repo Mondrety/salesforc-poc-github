9c03d955598488940b77b7cd5c7225c074bce2e7
n508829 <venkatababi.mondrety@ba.com>
2023-05-18 12:31:08 +0530

    Git lab Poc for merge

---
+++ scripts/sh/devsync.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+project_url=$(echo $CI_PROJECT_URL | sed 's/https:\/\///')
+git remote set-url origin https://oauth2:$GITLAB_ACCESS_TOKEN@$project_url
+
+git config user.name "Venkata Babi"
+git config user.email "venkatababi.mondrety@ba.com"
+
+git fetch
+
+git checkout branch1
+git log --no-merges --exit-code origin/demo ^HEAD || git merge --no-ff remotes/origin/demo
+git push origin branch1
+
+git checkout branch2
+git log --no-merges --exit-code origin/demo ^HEAD || git merge --no-ff remotes/origin/demo
+git push origin branch2
